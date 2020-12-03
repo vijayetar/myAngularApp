75c6f94028f08d68cccf688a3f997944f1eb3e16
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-12-02 21:07:36 -0800

    built course and courses component

---
+++ src/app/courses.component.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core';
+import {CoursesService} from './courses.service';
+
+@Component({
+  selector:'courses',
+  template:`
+  <h2>{{getTitle()}}</h2>
+  <ul>
+    <li *ngFor="let course of courses">{{course}}</li>
+  </ul>
+  `
+})
+export class CoursesComponent {
+  title = "List of courses are here!";
+  getTitle(){
+    return "Title from function: "+ this.title;
+  }
+  courses;
+  constructor (service: CoursesService){
+    this.courses = service.getCourses();
+  }
+}
